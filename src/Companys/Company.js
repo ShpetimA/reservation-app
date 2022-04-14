b0e696b6fdcccad972b88e15b59f0d2f744964b7
Shpetim Alimi <shpetim.alimi@ndbit.net>
2022-04-14 17:32:09 +0200

    Final design changes

--- src/Companys/Company.js
+++ src/Companys/Company.js
@@ -1,82 +1,107 @@
-import { Box } from '@mui/material'
-import {React,useState} from 'react'
-import TimeSlots from './TimeSlots'
-import {formatTime} from '../Utility/timeFunctions';
+import { Box } from '@mui/material';
+import { React, useState } from 'react';
+import TimeSlots from './TimeSlots';
+import { formatTime } from '../Utility/timeFunctions';
 
 const container = {
-   display: 'flex',
-   flexDirection: 'column',
-   gap: '1rem',
-   alignItems: 'center',
-   justifyContent: 'center'
-}
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
 
-const timeSlots = {
+const timeSlotsContainer = {
   width: '15rem',
   height: '40rem',
-  overflow: 'scroll',
+  overflowY: 'scroll',
+};
+
+const timeSlots = {
+  backgroundColor: 'aliceblue',
+  justifyContent: 'center',
   display: 'flex',
-  justifyContent: 'center'
-}
+  alignItems: 'center',
+  flexDirection: 'column',
+};
 
 const companyName = {
   padding: '2rem',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  backgroundColor: 'red',
+  backgroundColor: '#bac8ff',
   height: '5rem',
-  width: '100%'
-}
-
+};
 
 const Company = (props) => {
+  const [reservation, setReservation] = useState({});
+  const { name, time_slots } = props.company;
+  const weekDays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+  const [selected, setSelected] = useState({});
 
-  const [reservation,setReservation] = useState({});
-  const {name,time_slots} = props.company;
-  const weekDays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-  const [selected,setSelected] = useState({});
+  const filterTimeSlots = (time_slots, day) => {
+    return time_slots.filter((time_slot) => {
+      const [weekDay] = [
+        new Date(time_slot.start_time).toLocaleDateString('en-US', {
+          weekday: 'long',
+        }),
+      ];
+      return weekDay === day;
+    });
+  };
 
-  const filterTimeSlots = (time_slots,day) => {
-    return time_slots.filter((time_slot)=>{
-      const [weekDay] = [new Date(time_slot.start_time).toLocaleDateString('en-US',{weekday: 'long'})]
-      return weekDay === day 
-    })
-  }
-
-  const onReservation = (val,company,index,day) => {
-    if(Object.keys(selected).length === 0){
+  const onReservation = (val, company, index, day) => {
+    if (Object.keys(selected).length === 0) {
       setReservation(val);
-      setSelected({ index: index , day: day});
-      props.onReservation(val,company)
-    }else{
+      setSelected({ index: index, day: day });
+      props.onReservation(val, company);
+    } else {
       setSelected({});
       setReservation({});
-      props.onReservation({},company);
+      props.onReservation({}, company);
     }
-  }
+  };
 
   return (
     <Box sx={container}>
       <Box sx={companyName}>
-          <h3>{name}</h3>
+        <h3>{name}</h3>
       </Box>
       <Box>
-        { !reservation.start_time ? 'Reservation': `${formatTime(reservation.start_time)} - ${formatTime(reservation.end_time)}` }
+        {!reservation.start_time
+          ? 'Reservation'
+          : `${formatTime(reservation.start_time)} - ${formatTime(
+              reservation.end_time
+            )}`}
       </Box>
-      <Box sx={timeSlots}>
-        <ul>
-        {weekDays.map((day)=>{
-          return <div key={day}><h4>
-            {day}
-          </h4>
-          <TimeSlots  time_slots={filterTimeSlots(time_slots,day)} company={name} onReservation={onReservation} reservations={props.reservations} day={day}  selected={selected}></TimeSlots> 
-          </div>
-        })}  
-        </ul>
+      <Box sx={timeSlotsContainer}>
+        {weekDays.map((day) => {
+          return (
+            <Box sx={timeSlots} key={day}>
+              <h4>{day}</h4>
+              <TimeSlots
+                time_slots={filterTimeSlots(time_slots, day)}
+                company={name}
+                onReservation={onReservation}
+                reservations={props.reservations}
+                day={day}
+                selected={selected}
+              ></TimeSlots>
+            </Box>
+          );
+        })}
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
--- src/Companys/CompanyItems.js
+++ src/Companys/CompanyItems.js
@@ -1,39 +1,49 @@
-import React from 'react'
+import React from 'react';
 import Company from './Company';
 import { Box } from '@mui/material';
 import { useState } from 'react';
 
 const container = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems:'center',
   width: '120rem',
   height: '80vh',
-  gap: '2rem'
-}
+  gap: '2rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
 
+const appointmentStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: '#bac8ff',
+  boxShadow: '0px 0px 5px 2px rgba(0, 0, 0, .2);',
+};
 
 const CompanyItems = (props) => {
-  const {companys} = props;
-  const [reservations,setReservations] = useState({});
+  const { companys } = props;
+  const [reservations, setReservations] = useState({});
 
-  const onReserve = (val,comp) => {
-    setReservations({...reservations,[comp] : val});
-  }
+  const onReserve = (val, comp) => {
+    setReservations({ ...reservations, [comp]: val });
+  };
 
   return (
     <Box sx={container}>
-
-    {companys.map((company)=>{
-      return (
-        <Company key={company.name} company={company} onReservation={onReserve} reservations={reservations} ></Company>
-        )
-      }  
-    )}
-
+      <Box sx={appointmentStyle}>
+        {companys.map((company) => {
+          return (
+            <Company
+              key={company.name}
+              company={company}
+              onReservation={onReserve}
+              reservations={reservations}
+            ></Company>
+          );
+        })}
+      </Box>
     </Box>
-    
-  )
-}
+  );
+};
 
-export default CompanyItems
+export default CompanyItems;
